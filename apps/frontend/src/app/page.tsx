fafbc1e2767fda3c974c7f4502c276d80001b9b3
Welton Almeida de Matos <wamatos7@gmail.com>
2024-12-14 20:44:57 -0300

    feat : login page

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,101 +1,89 @@
+'use client'
+
+import { IconBrandGoogleFilled, IconEye, IconEyeOff, IconMail } from "@tabler/icons-react";
 import Image from "next/image";
+import { useState } from "react";
 
-export default function Home() {
+export default function Autenticacao() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [emailFocus, setEmailFocus] = useState(false);
+  const [passwordFocus, setPasswordFocus] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(prevState => !prevState);
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center px-10 py-5 rounded-md bg-zinc-900 select-none">
+        <Image src="/logo.png" alt="Logo" width={150} height={150} />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+        <span>Entre com sua conta</span>
+
+        <form className="flex flex-col w-full gap-2 mt-4">
+          <div className="flex flex-col gap-1 relative">
+            <label htmlFor="email" className="text-xs text-zinc-400">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="p-2 bg-black rounded-md border border-white/10 text-sm text-white"
+              onFocus={() => setEmailFocus(true)} 
+              onBlur={() => setEmailFocus(false)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <IconMail className={`absolute right-3 top-7 ${emailFocus ? 'text-zinc-400' : 'text-zinc-600'}`} />
+          </div>
+
+          <div className="flex flex-col gap-1 relative">
+            <label htmlFor="password" className="text-xs text-zinc-400">Senha</label>
+            <input
+              type={passwordVisible ? "text" : "password"}
+              id="password"
+              name="password"
+              className="p-2 bg-black rounded-md border border-white/10 text-sm text-white"
+              onFocus={() => setPasswordFocus(true)} 
+              onBlur={() => setPasswordFocus(false)}
+            />
+            {passwordVisible ? (
+              <IconEye
+                onClick={togglePasswordVisibility}
+                className={`absolute right-3 top-7 ${passwordFocus ? 'text-zinc-400' : 'text-zinc-600'} cursor-pointer`}
+              />
+            ) : (
+              <IconEyeOff
+                onClick={togglePasswordVisibility}
+                className={`absolute right-3 top-7 ${passwordFocus ? 'text-zinc-400' : 'text-zinc-600'} cursor-pointer`}
+              />
+            )}
+          </div>
+
+          <button className="text-xs text-zinc-500 ml-auto">Esqueceu sua senha?</button>
+
+          <button className="bg-green-500 rounded-md text-sm py-1">Login</button>
+
+          <div className="flex items-center justify-center my-2">
+            <div className="border-t border-zinc-800 w-full"></div>
+            <span className="mx-2 text-sm text-zinc-500">ou</span>
+            <div className="border-t border-zinc-800 w-full"></div>
+          </div>
+
+          <div className="flex flex-col gap-1 justify-center items-center">
+            <button className="bg-red-500 rounded-full w-10 h-10 flex items-center justify-center text-white">
+              <IconBrandGoogleFilled />
+            </button>
+
+            <div className="flex gap-1 text-sm mt-2">
+              <span className="text-zinc-300">Ainda não possui uma conta?</span> 
+
+              <button className="text-green-500">Cadastre-se <span className="underline">aqui</span></button>
+            </div>
+
+            <span className="text-xs text-zinc-500">
+              ou faça login pelo Google clicando no G acima.
+            </span>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
