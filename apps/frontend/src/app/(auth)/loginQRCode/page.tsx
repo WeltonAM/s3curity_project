304dff259141c40d35c546737311bf2ebf9f75c0
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2025-01-31 08:32:57 -0300

    Refatorando Login QRCode

--- apps/frontend/src/app/(auth)/loginQRCode/page.tsx
+++ apps/frontend/src/app/(auth)/loginQRCode/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import useAuth from "@/data/hooks/useAuth";
 import Carregando from "@/components/shared/Carregando";
@@ -9,16 +9,31 @@ export default function LoginQRCode() {
   const searchParams = useSearchParams();
   const router = useRouter();
   const { loginQr } = useAuth();
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   useEffect(() => {
-    const token = searchParams.get("token");
+    const autenticar = async () => {
+      const token = searchParams.get("token");
 
-    if (token) {
-      loginQr(token);  
-    } else {
-      router.push("/login");  
-    }
-  }, [searchParams, router, loginQr]);
+      if (!token) {
+        router.push("/login");
+        return;
+      }
 
-  return <Carregando />; 
+      if (!isAuthenticating) {
+        setIsAuthenticating(true);
+        try {
+          await loginQr(token);  
+          router.push("/");
+        } catch (error) {
+          console.error("Erro ao autenticar:", error);
+          router.push("/login");
+        }
+      }
+    };
+
+    autenticar();
+  }, [searchParams, router, loginQr, isAuthenticating]);
+
+  return <Carregando />;
 }
